db2edcdf3278de35dd1686076a6af749153b2705
TytoCorvus <brycecollins0@gmail.com>
2020-10-31 10:17:23 -0600

    Resolving a small error

--- src/main/java/com/bcollins/TwilioTest.java
+++ src/main/java/com/bcollins/TwilioTest.java
@@ -8,13 +8,16 @@ public class TwilioTest {
     // and set the environment variables. See http://twil.io/secure
     public static final String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
     public static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
+    public static final String MY_PHONE = System.getenv("MY_PHONE");
+    public static final String TWILIO_PHONE = System.getenv("TWILIO_PHONE");
+
 
     public TwilioTest(){}
 
     public void run(String message) {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
         Message text = Message.creator(
-                new com.twilio.type.PhoneNumber("+19166261463"),
+                new com.twilio.type.PhoneNumber(MY_PHONE),
                 new com.twilio.type.PhoneNumber("+18643438319"),
                 message)
                 .create();
